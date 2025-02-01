325881ef373975cc49d802b047cdeb769fa119b2
Saneckeg <shredderpast@gmail.com>
2025-02-01 10:25:18 +0300

    Update main.go

--- SecondProject/main.go
+++ SecondProject/main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
+var task string
+
+type requestBody struct {
+	Message string `json:"Message"`
+}
+
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/hello", helloHandler).Methods("GET")
+	router.HandleFunc("/api/hello", helloHandler).Methods("POST")
+	router.HandleFunc("/api/get-task", getTaskHundler).Methods("GET")
 	http.ListenAndServe(":8080", router)
 
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Здарова, дядя Саша")
+	var req requestBody
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+	task = req.Message
+
+	fmt.Fprintf(w, "Таска обновлена: %s", task)
+
+}
+
+func getTaskHundler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "НАША ЗАДАЧА ЭТО: %s", task)
 }
