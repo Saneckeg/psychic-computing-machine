c13139591dbce31b71dd0d4dd0bd2d639108ddb7
Saneckeg <shredderpast@gmai.com>
2025-02-08 09:42:11 +0300

    Добавил метод patch и delete

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -9,8 +10,8 @@ import (
 var task string
 
 type MessageResponse struct {
-	Task   string `json:"task"`
-	IsDone bool   `json:"is_done"`
+	Task string `json:"task"`
+	Id   bool   `json:"id"`
 }
 
 func main() {
@@ -22,6 +23,8 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/messages", CreateMessage).Methods("POST")
 	router.HandleFunc("/api/messages", GetMessages).Methods("GET")
+	router.HandleFunc("/api/messages{id}", PatchMessages).Methods("PATCH")
+	router.HandleFunc("/api/messages{id}", DeleteMessages).Methods("DELETE")
 	http.ListenAndServe(":8080", router)
 
 }
@@ -57,4 +60,52 @@ func GetMessages(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	json.NewEncoder(w).Encode(messages)
+}
+
+func PatchMessages(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var message Message
+	if err := DB.First(&message, id).Error; err != nil {
+		http.Error(w, "Message not found", http.StatusNotFound)
+		return
+	}
+
+	var updates map[string]interface{}
+	err := json.NewDecoder(r.Body).Decode(&updates)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	result := DB.Model(&message).Updates(updates)
+	if result.Error != nil {
+		http.Error(w, "Ошибка обновления в БД", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(message)
+}
+
+func DeleteMessages(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var message Message
+	if err := DB.First(&message, id).Error; err != nil {
+		http.Error(w, "Message not found", http.StatusNotFound)
+		return
+	}
+
+	if err := DB.Delete(&message).Error; err != nil {
+		http.Error(w, "Ошибка удаления", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "Message deleted")
 }
